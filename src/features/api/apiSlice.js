4d86f76ccbd1672b3a6aaad345f86fd2f4125e4a
rejoanafridi <rejoanafridi93@gmail.com>
2023-09-07 20:22:28 +0600

    server url

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
 	reducerPath: "api",
 	baseQuery: fetchBaseQuery({
-		baseUrl: "http://localhost:9000",
+		baseUrl: "https://task-management-server-vwl5.onrender.com/",
 	}),
 	tagTypes: [],
 	endpoints: (builder) => ({}),
